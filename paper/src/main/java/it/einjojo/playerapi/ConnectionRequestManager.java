9a908532666517f5316ade25a9bcec955d4f15e2
Johannes / EinJojo <admin@einjojo.it>
2025-11-14 08:31:29 +0100

    Refactor `ConnectionRequestManager` to use `LinkedBlockingDeque` for pending requests, add expiration logic, and replace `ConcurrentHashMap` with custom `PendingRequest` class.

--- paper/src/main/java/it/einjojo/playerapi/ConnectionRequestManager.java
+++ paper/src/main/java/it/einjojo/playerapi/ConnectionRequestManager.java
@@ -4,15 +4,15 @@ import io.lettuce.core.api.StatefulRedisConnection;
 import it.einjojo.protocol.player.ConnectRequest;
 import it.einjojo.protocol.player.ConnectResponse;
 import org.jspecify.annotations.NullMarked;
+import org.jspecify.annotations.Nullable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.security.SecureRandom;
-import java.util.Map;
 import java.util.Objects;
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.LinkedBlockingDeque;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Consumer;
 
@@ -21,7 +21,7 @@ public class ConnectionRequestManager implements Consumer<ConnectResponse> {
     private static final SecureRandom random = new SecureRandom();
     private static final Logger log = LoggerFactory.getLogger(ConnectionRequestManager.class);
     private final StatefulRedisConnection<byte[], byte[]> connection;
-    private final Map<Integer, CompletableFuture<ServerConnectResult>> pendingRequests = new ConcurrentHashMap<>();
+    private final LinkedBlockingDeque<PendingRequest> pendingRequests = new LinkedBlockingDeque<>();
 
 
     public ConnectionRequestManager(RedisPubSubHandler pubSubHandler) {
@@ -36,7 +36,7 @@ public class ConnectionRequestManager implements Consumer<ConnectResponse> {
      */
     @Override
     public void accept(ConnectResponse connectResponse) {
-        var future = pendingRequests.get(connectResponse.getResponseKey());
+        var future = pop(connectResponse.getResponseKey());
         if (future == null) {
             log.warn("No future found for response key {}", connectResponse.getResponseKey());
             return;
@@ -51,6 +51,28 @@ public class ConnectionRequestManager implements Consumer<ConnectResponse> {
         future.complete(res);
     }
 
+    /**
+     * Looks into the pending requests and removes the element associated with the response key.
+     * On the iteration all expired elements are removed.
+     *
+     * @param responseKey the response to a connection request
+     * @return the removed element, or null if not found
+     */
+    private @Nullable CompletableFuture<ServerConnectResult> pop(int responseKey) {
+        for (var it = pendingRequests.iterator(); it.hasNext(); ) {
+            var pendingRequest = it.next();
+            if (pendingRequest.isExpired()) {
+                it.remove();
+                continue;
+            }
+            if (pendingRequest.responseKey == responseKey) {
+                it.remove();
+                return pendingRequest.future;
+            }
+        }
+        return null;
+    }
+
     public void fireAndForget(UUID uuid, String serviceName) {
         connection.sync().publish(RedisPubSubHandler.CONNECT_REQ_CHANNEL, ConnectRequest.newBuilder()
                 .setUniqueId(uuid.toString())
@@ -61,12 +83,21 @@ public class ConnectionRequestManager implements Consumer<ConnectResponse> {
     public CompletableFuture<ServerConnectResult> newRequest(UUID uuid, String serviceName) {
         int responseKey = random.nextInt(Integer.MAX_VALUE);
         CompletableFuture<ServerConnectResult> future = new CompletableFuture<>();
-        pendingRequests.put(responseKey, future);
+        pendingRequests.add(new PendingRequest(responseKey, future, System.currentTimeMillis() + 10_000));
         connection.sync().publish(RedisPubSubHandler.CONNECT_REQ_CHANNEL, ConnectRequest.newBuilder()
                 .setUniqueId(uuid.toString())
                 .setServerName(serviceName)
                 .setResponseKey(responseKey)
                 .build().toByteArray());
         return future.orTimeout(10, TimeUnit.SECONDS).whenComplete((result, throwable) -> pendingRequests.remove(responseKey));
+
+
+    }
+
+    private record PendingRequest(int responseKey, CompletableFuture<ServerConnectResult> future, long expiryTime) {
+        public boolean isExpired() {
+            return expiryTime < System.currentTimeMillis();
+        }
+
     }
 }
